e60dd6df34abf99c5f52e65de156d87cf240ac40
Abdoll <96470320+YouGotTrolled@users.noreply.github.com>
2025-03-04 03:46:24 +0330

    up

---
+++ src/main/java/com/example/noor_bank/Noor.java
@@ -0,0 +1,44 @@
+package com.example.noor_bank;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
+public class Noor extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Noor.class.getResource("Main.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
+        Image ic = new Image("Noor.png");
+        stage.setMinHeight(720);
+        stage.setMinWidth(1280);
+        stage.getIcons().add(ic);
+        stage.setTitle("Noor Bank");
+        stage.setResizable(false);
+        scene.setOnMousePressed((MouseEvent event) ->{
+            x = event.getSceneX();
+            y = event.getSceneY();
+        });
+        scene.setOnMouseDragged((MouseEvent event) ->{
+            stage.setX(event.getScreenX() - x);
+            stage.setY(event.getScreenY() - y);
+            stage.setOpacity(.6);
+        });
+        scene.setOnMouseReleased((MouseEvent event) -> stage.setOpacity(1));
+        stage.initStyle(StageStyle.TRANSPARENT);
+        stage.setScene(scene);
+        stage.show();
+    }
+    private double x;
+    private double y;
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/com/example/noor_bank/mainController.java
@@ -0,0 +1,13 @@
+package com.example.noor_bank;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class mainController {
+
+    @FXML
+    void exitApp(ActionEvent event) {
+        System.exit(0);
+    }
+
+}
